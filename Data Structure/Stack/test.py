49b4bcfd3bb095c7d3cf2b36e3ee5bb9bfced3b3
Shaon <shamim.hasan@gagagugu.com>
2017-03-22 15:40:06 +0600

    Stack code added

---
+++ Data Structure/Stack/test.py
@@ -0,0 +1,13 @@
+from stack import Stack
+
+stack = Stack()
+
+stack.push(10)
+stack.push(15)
+
+print(stack.isEmpty())
+print(stack.peek())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.isEmpty())
